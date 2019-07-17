52b993c3d6a4c4f261493db5158052801dfaf744
Dernival Liandro <39772165+Liandro73@users.noreply.github.com>
2019-07-17 02:40:51 -0300

    Add files via upload

---
+++ Aula05.java
@@ -0,0 +1,26 @@
+package br.com.liandro.hhankatividades;
+
+import java.util.Scanner;
+
+public class Aula05 {
+	
+	private static final Scanner ler = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        long N = ler.nextLong();
+        ler.close();
+         if(N % 2 != 0) {
+            System.out.println("Weird");
+        }        
+        if((N > 2 & N < 5) & N % 2 == 0) {
+            System.out.println("Not Weird");
+        }
+        if(N > 6 & N <= 20) {
+            System.out.println("Weird");
+        }
+        if (N > 20 & N % 2 == 0) {
+            System.out.println("Not Weird");
+        }
+    }
+}
+
